97cff698f95ea8376989d9738903da3ba2a88942
nirch <nirh2@yahoo.com>
2026-06-28 09:39:39 +0300

    Step 1: Components & JSX (Hard Coded)

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,10 +1,12 @@
+import Movie from '../components/Movie'
 import './MoviesPage.css'
 
 export default function MoviesPage() {
   return (
     <div className='movies-page'>
       <h1>Movies Page</h1>
-
+      <Movie />
+      <Movie />
     </div>
   )
 }
