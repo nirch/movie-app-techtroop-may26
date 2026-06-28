dbb972cd61f749ed57988abaec76fc88f12b7498
nirch <nirh2@yahoo.com>
2026-06-28 10:03:24 +0300

    Movie comp get movie as prop

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,12 +1,22 @@
-import Movie from '../components/Movie'
-import './MoviesPage.css'
+import Movie from "../components/Movie";
+import "./MoviesPage.css";
 
 export default function MoviesPage() {
+  const movie = {
+    name: "Forrest Gump",
+    runtime: 142,
+    year: 1994,
+    plot: "The presidencies of Kennedy and Johnson, the Vietnam War, the Watergate scandal and other historical events unfold from the perspective of an Alabama man with an IQ of 75, whose only desire is to be reunited with his childhood sweetheart.",
+    genres: ["Drama", "Comedy"],
+    poster:
+      "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg",
+    imdb: "https://www.imdb.com/title/tt0109830/",
+  };
   return (
-    <div className='movies-page'>
+    <div className="movies-page">
       <h1>Movies Page</h1>
-      <Movie />
-      <Movie />
+      <Movie movie={movie}/>
+      <Movie movie={movie}/>
     </div>
-  )
+  );
 }
