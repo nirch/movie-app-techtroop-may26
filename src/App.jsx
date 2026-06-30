2b119a2101e8a9e9fa99d12521bd161819e7e4d6
nirch <nirh2@yahoo.com>
2026-06-30 09:52:24 +0300

    mantine + ActorsPage simple

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import { useState } from 'react'
 import './App.css'
 import MoviesPage from './pages/MoviesPage'
+import ActorsPage from './pages/ActorsPage'
 
 function App() {
 
   return (
     <>
+      <ActorsPage />
       <MoviesPage />
     </>
   )
---
+++ src/pages/ActorsPage.jsx
@@ -0,0 +1,9 @@
+import { Container } from "@mantine/core";
+
+export default function ActorsPage() {
+  return (
+    <Container size="md">
+      <h1>Actors Page</h1>
+    </Container>
+  )
+}
